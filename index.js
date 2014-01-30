e5d9a8d2e25edb892485f6e62105da107e211709
athieme <Gr8v1yard>
2014-01-30 13:18:17 -0800

    attempting to use local ping server urls

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var InternalOAuthError = require('passport-oauth').InternalOAuthError;
 /**
  * `Strategy` constructor.
  *
- * The GitHub authentication strategy authenticates requests by delegating to
- * GitHub using the OAuth 2.0 protocol.
+ * The Ping authentication strategy authenticates requests by delegating to
+ * PingFederate using the OAuth 2.0 protocol.
  *
  * Applications must supply a `verify` callback which accepts an `accessToken`,
  * `refreshToken` and service-specific `profile`, and then calls the `done`
@@ -49,68 +49,17 @@ var InternalOAuthError = require('passport-oauth').InternalOAuthError;
  */
 function Strategy(options , verify) {
     options = options || {};
-    options.authorizationURL = options.authorizationURL || 'https://github.com/login/oauth/authorize';
-    options.tokenURL = options.tokenURL || 'https://github.com/login/oauth/access_token';
-    options.scopeSeparator = options.scopeSeparator || ',';
-    options.customHeaders = options.customHeaders || {};
-
-    if (!options.customHeaders['User-Agent']) {
-        options.customHeaders['User-Agent'] = options.userAgent || 'passport-github';
-    }
+    options.authorizationURL = options.authorizationURL || 'https://localhost:9031/as/authorization.oauth2';
+    options.tokenURL = options.tokenURL || 'https://localhost:9031/as/token.oauth2';
+    options.skipUserProfile = true;
 
     OAuth2Strategy.call(this , options , verify);
     this.name = 'ping';
-    this._userProfileURL = options.userProfileURL || 'https://api.github.com/user';
 };
 
 /**
  * Inherit from `OAuth2Strategy`.
  */
 util.inherits(Strategy , OAuth2Strategy);
-
-
-/**
- * Retrieve user profile from GitHub.
- *
- * This function constructs a normalized profile, with the following properties:
- *
- *   - `provider`         always set to `github`
- *   - `id`               the user's GitHub ID
- *   - `username`         the user's GitHub username
- *   - `displayName`      the user's full name
- *   - `profileUrl`       the URL of the profile for the user on GitHub
- *   - `emails`           the user's email addresses
- *
- * @param {String} accessToken
- * @param {Function} done
- * @api protected
- */
-Strategy.prototype.userProfile = function (accessToken , done) {
-    this._oauth2.get(this._userProfileURL , accessToken , function (err , body , res) {
-        if (err) {
-            return done(new InternalOAuthError('failed to fetch user profile' , err));
-        }
-
-        try {
-            var json = JSON.parse(body);
-
-            var profile = { provider : 'github' };
-            profile.id = json.id;
-            profile.displayName = json.name;
-            profile.username = json.login;
-            profile.profileUrl = json.html_url;
-            profile.emails = [
-                { value : json.email }
-            ];
-
-            profile._raw = body;
-            profile._json = json;
-
-            done(null , profile);
-        } catch (e) {
-            done(e);
-        }
-    });
-};
 
 exports.Strategy = Strategy;
